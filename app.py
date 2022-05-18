c10906ad9a8355b7dc6e19e39471d465df6be829
Shaheen Khan <fshaheenkhan@gmail.com>
2022-05-18 10:42:27 -0400

    fixed apikey

--- app.py
+++ app.py
@@ -28,6 +28,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = (
     os.environ.get('DATABASE_URL', 'postgresql:///book_app'))
 
+key = os.getenv("API_KEY", "optional-default")
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
